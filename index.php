7f25d9f85cc1d4e16cf1b50765c64fcd616bba18
Salvatore Trimarchi <path72@gmail.com>
2021-04-27 15:39:58 +0200

    display items

--- index.php
+++ index.php
@@ -77,6 +77,31 @@
 
 
 
+				<!-- ** ITEM DISPLAY PANEL ** -->
+				<div class="item_display flex-row-co fx-wrap">
+					
+					<?php foreach ($database as $item) { ?>
+						<div>
+							<div class="item_box">
+								<div class="card flex-col">
+									<img src="<?php echo $item['poster'] ?>" alt="">
+									<div class="item_title fx-grow-1 txt_1">
+										<?php echo $item['title'] ?>
+									</div>
+									<div class="item_info flex-col-co">
+										<div class="item_author txt_2">
+											<?php echo $item['author'] ?>
+										</div>
+										<div class="item_year txt_2">
+											<?php echo $item['year'] ?>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					<?php } ?>
+
+				</div>
 
 
 
@@ -93,7 +118,6 @@
 
 
 
-				
 				<!-- ** ITEM DISPLAY PANEL ** -->
 				<!-- <div class="item_display flex-row-co fx-wrap" v-if="displayItemsAreReady">
 					<div v-for="item in displayItems" v-if="isViewable(item)">
